910e818d8a1effd09ddfe13d3f85dff19d6cc97a
Navya bollareddy <bollareddynavya@gmail.com>
2026-01-28 19:10:55 +0530

    Add backup vault and plan names to main.tf

--- backup_terraform/env/dev/main.tf
+++ backup_terraform/env/dev/main.tf
@@ -4,6 +4,8 @@ module "ec2_backup" {
   ami_id        = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
   backup_role_name  = "terraform-dev-backup-role"
+  backup_vault_name = "terraform-dev-backup-vault"
+  backup_plan_name  = "terraform-dev-backup-plan"
 
   tags = {
     Name = "terraform-vm"
