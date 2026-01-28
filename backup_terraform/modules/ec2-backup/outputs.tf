29d523b0528e0425a4080236824d4092db5b2241
navya <navya@gmail.com>
2026-01-28 09:59:36 +0000

    add backup module

---
+++ backup_terraform/modules/ec2-backup/outputs.tf
@@ -0,0 +1,32 @@
+output "instance_id" {
+  value = aws_instance.vm.id
+}
+
+output "backup_vault_name" {
+  value = aws_backup_vault.vault.name
+}
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                  0,1           All
+
