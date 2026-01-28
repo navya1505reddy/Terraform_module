07dab7b1ec57875113a954a5583b621ec74e38dd
Navya bollareddy <bollareddynavya@gmail.com>
2026-01-28 16:42:09 +0530

    Update main.tf

--- backup_terraform/env/dev/main.tf
+++ backup_terraform/env/dev/main.tf
@@ -3,6 +3,7 @@ module "ec2_backup" {
 
   ami_id        = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
+  backup_role_name  = "terraform-dev-backup-role"
 
   tags = {
     Name = "terraform-vm"
